0a30df83c8e104abb376eac4131d07cac3db821d
Philip Rasmusson <studiobigmouth@gmail.com>
2021-07-16 18:47:38 +0200

    started on mobile view

--- src/components/header/headerMobile/HeaderMobile.tsx
+++ src/components/header/headerMobile/HeaderMobile.tsx
@@ -1,9 +1,23 @@
 import './HeaderMobile.css'
+import headerImg from '../../../shared/img/IMG_0085.png'
+import { textEnglish } from '../../../data/TextEnglish'
+import { SocialMediaIcons } from "../../socialMedia/SocialMediaIcons"
+
 
 export const HeaderMobile = () => {
   return (
-    <div>
-      hjhj
+    <div className="header-mobile">
+      <div className="main-line-mobile"></div>
+      <div className="title-wrapper-mobile">
+        <h1 className="title-name-mobile font-upper">{textEnglish.titleName}</h1>
+        <h3 className="title-desc-mobile font-upper">{textEnglish.titleDescSwe}</h3>
+      </div>
+      <div className="title-img-mobile">
+        <img src={headerImg} alt="Profile picture" />
+      </div>
+      <div className="social-media-mobile-wrapper hidden">
+        {SocialMediaIcons()}
+      </div>
     </div>
   )
 }
--- src/components/navigation/mobileNavigation/MobileNavigation.tsx
+++ src/components/navigation/mobileNavigation/MobileNavigation.tsx
@@ -5,39 +5,24 @@ import { MenuItems } from "../MenuItems"
 
 export const MobileNavigation = () => {
 
-  const [burgerIcon, setBurgerIcon] = useState<string>('burger')
-  const [menuTrans, setMenuTrans] = useState<any>('-20vw')
-
-  const burgerAnimation = (input: string) => {
-
-    return `${burgerIcon}-icon-${input}`
-  }
-
-  const menuSlideIn = () => {
-    return burgerIcon === 'burger' ? { right: '-100vw' } : { right: '0' }
-  }
-
-
-  const displayBurgerIcon = () => {
-    burgerIcon === 'burger' ? setBurgerIcon('times') : setBurgerIcon('burger')
-  }
-
 
   return (
     <div>
-      <div className='navbar-mobile-wrapper'>
-        <div className='navbar-mobile-wrapper-inner font-black'>
-          <div className='navbar-mobile-logo'></div>
-          <div className={burgerAnimation('wrapper')} onClick={() => displayBurgerIcon()}>
-            <div className={burgerAnimation('line-1')}></div>
-            <div className={burgerAnimation('line-2')}></div>
-            <div className={burgerAnimation('line-3')}></div>
-          </div>
-          <div className='navbar-mobile-menu-wrapper' style={menuSlideIn()}>
-            {MenuItems()}
-          </div>
-        </div>
+      <div className='navbar-mobile-wrapper hidden'>
+        {MenuItems()}
       </div>
     </div>
   )
 }
+
+        // <div className='navbar-mobile-wrapper-inner font-black'>
+        //   <div className='navbar-mobile-logo'></div>
+        //   <div className={burgerAnimation('wrapper')} onClick={() => displayBurgerIcon()}>
+        //     <div className={burgerAnimation('line-1')}></div>
+        //     <div className={burgerAnimation('line-2')}></div>
+        //     <div className={burgerAnimation('line-3')}></div>
+        //   </div>
+        //   <div className='navbar-mobile-menu-wrapper' style={menuSlideIn()}>
+        //     {MenuItems()}
+        //   </div>
+        // </div>
